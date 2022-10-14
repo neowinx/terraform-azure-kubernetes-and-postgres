9a8ad2b2017847a163ce3c3de8f7d2c13604a143
Pedro Flores <pflores@dncp.gov.py>
2022-10-14 15:53:45 -0300

    testing postgresql creation from quickstart

--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "resource_group_name_prefix" {
   default     = "rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
+
+variable "name_prefix" {
+  default     = "postgresqlfs"
+  description = "Prefix of the resource name."
+}
